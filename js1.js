5aeed267e518e7f0616d1d22e7af815a19554c83
Daniel Dael <danieldael7777@gmail.com>
2024-01-22 23:20:58 +0800

    JS variables and objects

---
+++ js1.js
@@ -0,0 +1,15 @@
+let age = 32;
+let userName = "Daniel";
+let hobbies = ["Coding","Singing","Watching Anime"];
+
+let job = {
+    title:"CRM Developer",
+    place:"Makati",
+    salary: 50000,
+};
+
+
+
+console.log(job.title);
+alert(job.title);
+
